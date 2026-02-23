da200e0d44ee4239cee19a79f2dfe2bf80cb2ab0
SANTIAGO <santic93.sc@gmail.com>
2026-02-23 17:47:45 -0300

    features

--- src/features/booking/BookingPage.tsx
+++ src/features/booking/BookingPage.tsx
@@ -1,5 +1,6 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import dayjs, { Dayjs } from "dayjs";
+
 import { useState } from "react";
 import {
   DatePicker,
@@ -20,6 +21,7 @@ import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { db } from "../../lib/firebase"
 import { query, where, getDocs } from "firebase/firestore";
 import { useAuth } from "../../context/AuthContext";
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 export const BookingPage = () => {
   const { hostelSlug } = useAuth();
   const location = useLocation();
@@ -51,73 +53,73 @@ export const BookingPage = () => {
 
   const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(false);
-const checkAvailability = async () => {
-  if (!hostelSlug || !selectedRoom) return false;
+  const checkAvailability = async () => {
+    if (!hostelSlug || !selectedRoom) return false;
 
-  const q = query(
-    collection(db, "hostels", hostelSlug, "reservations"),
-    where("roomId", "==", selectedRoom.id)
-  );
-
-  const snapshot = await getDocs(q);
-
-  const newCheckIn = checkIn?.toDate();
-  const newCheckOut = checkOut?.toDate();
-
-  for (const doc of snapshot.docs) {
-    const data = doc.data();
-
-    const existingCheckIn = data.checkIn.toDate();
-    const existingCheckOut = data.checkOut.toDate();
-
-    const isOverlapping =
-      newCheckIn &&
-      newCheckIn < existingCheckOut &&
-      newCheckOut &&
-      newCheckOut > existingCheckIn;
-
-    if (isOverlapping) return false;
-  }
-
-  return true;
-};
- const handleConfirm = async () => {
-  if (!isFormValid || !hostelSlug || !selectedRoom) return;
-
-  try {
-    setLoading(true);
-
-    const available = await checkAvailability();
-
-    if (!available) {
-      alert("Selected dates are not available for this room.");
-      setLoading(false);
-      return;
-    }
-
-    await addDoc(
+    const q = query(
       collection(db, "hostels", hostelSlug, "reservations"),
-      {
-        roomId: selectedRoom.id,
-        roomName: selectedRoom.name,
-        pricePerNight: selectedRoom.price,
-        checkIn: checkIn?.toDate(),
-        checkOut: checkOut?.toDate(),
-        nights,
-        total,
-        fullName,
-        email,
-        createdAt: serverTimestamp(),
-      }
+      where("roomId", "==", selectedRoom.id)
     );
 
-    setSuccess(true);
-  } catch (error) {
-    console.error("Error saving reservation:", error);
-  } finally {
-    setLoading(false);
-  }
-};
+    const snapshot = await getDocs(q);
+
+    const newCheckIn = checkIn?.toDate();
+    const newCheckOut = checkOut?.toDate();
+
+    for (const doc of snapshot.docs) {
+      const data = doc.data();
+
+      const existingCheckIn = data.checkIn.toDate();
+      const existingCheckOut = data.checkOut.toDate();
+
+      const isOverlapping =
+        newCheckIn &&
+        newCheckIn < existingCheckOut &&
+        newCheckOut &&
+        newCheckOut > existingCheckIn;
+
+      if (isOverlapping) return false;
+    }
+
+    return true;
+  };
+  const handleConfirm = async () => {
+    if (!isFormValid || !hostelSlug || !selectedRoom) return;
+
+    try {
+      setLoading(true);
+
+      const available = await checkAvailability();
+
+      if (!available) {
+        alert("Selected dates are not available for this room.");
+        setLoading(false);
+        return;
+      }
+
+      await addDoc(
+        collection(db, "hostels", hostelSlug, "reservations"),
+        {
+          roomId: selectedRoom.id,
+          roomName: selectedRoom.name,
+          pricePerNight: selectedRoom.price,
+          checkIn: checkIn?.toDate(),
+          checkOut: checkOut?.toDate(),
+          nights,
+          total,
+          fullName,
+          email,
+          createdAt: serverTimestamp(),
+        }
+      );
+
+      setSuccess(true);
+    } catch (error) {
+      console.error("Error saving reservation:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
   if (success) {
     return (
       <Container sx={{ py: 15, textAlign: "center" }}>
@@ -133,115 +135,124 @@ const checkAvailability = async () => {
     );
   }
   if (!hostelSlug) {
-  return null; // o un loader si querés algo más elegante
-}
+    return null; // o un loader si querés algo más elegante
+  }
+  const navigate = useNavigate();
   return (
-    <Container sx={{ py: 10 }}>
-      <Typography variant="h2" gutterBottom>
-        Book Your Stay
-      </Typography>
+    <>
+      <Button
+        onClick={() => navigate(-1)}
+        startIcon={<ArrowBackIosNewIcon />}
+      >
+        Back
+      </Button>
+      <Container sx={{ py: 10 }}>
+        <Typography variant="h2" gutterBottom>
+          Book Your Stay
+        </Typography>
 
-      <Grid container spacing={8} sx={{ mt: 4 }}>
+        <Grid container spacing={8} sx={{ mt: 4 }}>
 
-        {/* LEFT COLUMN — FORM */}
-        <Grid size={{ xs: 12, md: 7 }}>
+          {/* LEFT COLUMN — FORM */}
+          <Grid size={{ xs: 12, md: 7 }}>
 
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <Stack spacing={4}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <Stack spacing={4}>
 
-              <DatePicker
-                label="Check-in"
-                value={checkIn}
-                minDate={dayjs()}
-                onChange={(newValue) => setCheckIn(newValue)}
+                <DatePicker
+                  label="Check-in"
+                  value={checkIn}
+                  minDate={dayjs()}
+                  onChange={(newValue) => setCheckIn(newValue)}
+                />
+
+                <DatePicker
+                  label="Check-out"
+                  value={checkOut}
+                  minDate={checkIn || dayjs()}
+                  onChange={(newValue) => setCheckOut(newValue)}
+                />
+
+              </Stack>
+
+            </LocalizationProvider>
+            <Stack spacing={4} sx={{ mt: 6 }}>
+              <TextField
+
+                label="Full Name"
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+                fullWidth
               />
 
-              <DatePicker
-                label="Check-out"
-                value={checkOut}
-                minDate={checkIn || dayjs()}
-                onChange={(newValue) => setCheckOut(newValue)}
+              <TextField
+                label="Email Address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                fullWidth
+                error={touched && !isEmailValid}
+                helperText={
+                  touched && !isEmailValid
+                    ? "Enter a valid email address"
+                    : ""
+                }
               />
-
             </Stack>
-
-          </LocalizationProvider>
-          <Stack spacing={4} sx={{ mt: 6 }}>
-            <TextField
-
-              label="Full Name"
-              value={fullName}
-              onChange={(e) => setFullName(e.target.value)}
+            <Button
+              variant="contained"
               fullWidth
-            />
+              sx={{ mt: 6, py: 2 }}
+              disabled={!isFormValid || loading}
+              onClick={handleConfirm}
+            >
+              {loading ? "Processing..." : "CONFIRM RESERVATION"}
+            </Button>
 
-            <TextField
-              label="Email Address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              fullWidth
-              error={touched && !isEmailValid}
-              helperText={
-                touched && !isEmailValid
-                  ? "Enter a valid email address"
-                  : ""
-              }
-            />
-          </Stack>
-          <Button
-            variant="contained"
-            fullWidth
-            sx={{ mt: 6, py: 2 }}
-            disabled={!isFormValid || loading}
-            onClick={handleConfirm}
-          >
-            {loading ? "Processing..." : "CONFIRM RESERVATION"}
-          </Button>
+          </Grid>
+
+          {/* RIGHT COLUMN — SUMMARY */}
+          <Grid size={{ xs: 12, md: 5 }}>
+
+            <Box
+              sx={{
+                border: "1px solid #E0E0E0",
+                borderRadius: 3,
+                p: 4,
+                position: { md: "sticky" },
+                top: { md: 100 },
+              }}
+            >
+
+              {selectedRoom ? (
+                <>
+                  <Typography variant="h5" gutterBottom>
+                    {selectedRoom.name}
+                  </Typography>
+
+                  <Typography sx={{ mb: 2 }}>
+                    ${selectedRoom.price} per night
+                  </Typography>
+
+                  <Box sx={{ borderTop: "1px solid #eee", my: 3 }} />
+
+                  <Typography>
+                    {nights > 0 ? `${nights} nights` : "Select dates"}
+                  </Typography>
+
+                  <Typography variant="h6" sx={{ mt: 2 }}>
+                    Total: ${total}
+                  </Typography>
+                </>
+              ) : (
+                <Typography>Select a room first.</Typography>
+              )}
+
+            </Box>
+
+          </Grid>
 
         </Grid>
-
-        {/* RIGHT COLUMN — SUMMARY */}
-        <Grid size={{ xs: 12, md: 5 }}>
-
-          <Box
-            sx={{
-              border: "1px solid #E0E0E0",
-              borderRadius: 3,
-              p: 4,
-              position: { md: "sticky" },
-              top: { md: 100 },
-            }}
-          >
-
-            {selectedRoom ? (
-              <>
-                <Typography variant="h5" gutterBottom>
-                  {selectedRoom.name}
-                </Typography>
-
-                <Typography sx={{ mb: 2 }}>
-                  ${selectedRoom.price} per night
-                </Typography>
-
-                <Box sx={{ borderTop: "1px solid #eee", my: 3 }} />
-
-                <Typography>
-                  {nights > 0 ? `${nights} nights` : "Select dates"}
-                </Typography>
-
-                <Typography variant="h6" sx={{ mt: 2 }}>
-                  Total: ${total}
-                </Typography>
-              </>
-            ) : (
-              <Typography>Select a room first.</Typography>
-            )}
-
-          </Box>
-
-        </Grid>
-
-      </Grid>
-    </Container>
+      </Container>
+    </>
   );
 };
--- src/features/rooms/RoomDetailPage.tsx
+++ src/features/rooms/RoomDetailPage.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { rooms } from "./rooms.data";
 import { Container, Typography, Box, Button } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 export const RoomDetailPage = () => {
   const { id } = useParams();
 
@@ -12,39 +13,49 @@ export const RoomDetailPage = () => {
   }
 
   return (
-    <Container sx={{ py: 10 }}>
-      <Box
-        component="img"
-        src={room.image}
-        sx={{
-          width: "100%",
-          maxHeight: 500,
-          objectFit: "cover",
-          borderRadius: 2,
-          mb: 6,
-        }}
-      />
-
-      <Typography variant="h2" gutterBottom>
-        {room.name}
-      </Typography>
-
-      <Typography sx={{ mb: 4 }}>
-        {room.description}
-      </Typography>
-
-      <Typography sx={{ mb: 4 }}>
-        From ${room.price} / night
-      </Typography>
-
+    <>
       <Button
-        variant="contained"
         component={RouterLink}
-        to="/booking"
-        state={{ room }}
+        to="/rooms"
+        startIcon={<ArrowBackIosNewIcon />}
       >
-        BOOK THIS ROOM
+        Back to Rooms
       </Button>
-    </Container>
+
+      <Container sx={{ py: 10 }}>
+
+        <Box
+          component="img"
+          src={room.image}
+          sx={{
+            width: "100%",
+            maxHeight: 500,
+            objectFit: "cover",
+            borderRadius: 2,
+            mb: 6,
+          }}
+        />
+
+        <Typography variant="h2" gutterBottom>
+          {room.name}
+        </Typography>
+
+        <Typography sx={{ mb: 4 }}>
+          {room.description}
+        </Typography>
+
+        <Typography sx={{ mb: 4 }}>
+          From ${room.price} / night
+        </Typography>
+
+        <Button
+          variant="contained"
+          component={RouterLink}
+          to="/booking"
+          state={{ room }}
+        >
+          BOOK THIS ROOM
+        </Button>
+      </Container> </>
   );
 };
